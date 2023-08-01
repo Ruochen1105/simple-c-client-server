9d740403330f237ab5c2006991cf649b32205866
Ruochen Miao <rm5327@nyu.edu>
2023-08-01 18:31:47 +0800

    rename

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-all: httpd client
+all: http_server client
 
-httpd: httpd.c
-	gcc -W -Wall -lpthread -o server httpd.c
+http_server: http_server.c
+	gcc -W -Wall -lpthread -o server http_server.c
 
 client: simpleclient.c
 	g++ -std=c++11 -o client simpleclient.c
@@ -9,4 +9,4 @@ client: simpleclient.c
 clean:
 	rm server client
 
-.SILENT: client httpd
+.SILENT: client http_server
---
+++ http_server.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+// #include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+/**********************************************************************/
+/* Print out an error message with perror() (for system errors; based
+ * on value of errno, which indicates system call errors) and exit the
+ * program indicating an error. */
+/**********************************************************************/
+void error_die(const char *sc)
+{
+ perror(sc);
+ exit(1);
+}
+
+/**********************************************************************/
+/* This function starts the process of listening for web connections
+ * on a specified port.  If the port is 0, then dynamically allocate a
+ * port and modify the original port variable to reflect the actual
+ * port.
+ * Parameters: pointer to variable containing the port to connect on
+ * Returns: the socket */
+/**********************************************************************/
+int startup(u_short *port)
+{
+ int httpd = 0;
+ struct sockaddr_in name;
+
+ httpd = socket(PF_INET, SOCK_STREAM, 0);
+ if (httpd == -1)
+  error_die("socket");
+ memset(&name, 0, sizeof(name));
+ name.sin_family = AF_INET;
+ name.sin_port = htons(4321);
+ name.sin_addr.s_addr = htonl(INADDR_ANY);
+ if (bind(httpd, (struct sockaddr *)&name, sizeof(name)) < 0)
+  error_die("bind");
+ if (*port == 0)  /* if dynamically allocating a port */
+ {
+  socklen_t namelen = sizeof(name);
+  if (getsockname(httpd, (struct sockaddr *)&name, &namelen) == -1)
+   error_die("getsockname");
+  *port = ntohs(name.sin_port);
+ }
+ if (listen(httpd, 5) < 0)
+  error_die("listen");
+ return(httpd);
+}
+
+int main(void)
+{
+ int server_sock = -1;
+ u_short port = 0;
+ int client_sock = -1;
+ struct sockaddr_in client_name;
+ socklen_t client_name_len = sizeof(client_name);
+
+ server_sock = startup(&port);
+ printf("httpd running on port %d\n", port);
+
+ while (1)
+ {
+  client_sock = accept(server_sock,
+                       (struct sockaddr *)&client_name,
+                       &client_name_len);
+  if (client_sock == -1) {
+   error_die("accept");
+  }
+  sleep(1);
+  char buf[1024];
+
+  strcpy(buf, "HTTP/1.1 200 OK\r\nfoo: bar\r\nContent-type: text/html\r\nContent-Length: 11\r\n\r\nLorem Ipsum");
+  send(client_sock, buf, strlen(buf), 0);
+ }
+}
